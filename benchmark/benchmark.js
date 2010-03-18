18f13455d3c8be32aa451f8e4a6a3bb9653bcfff
Oleg Efimov <efimovov@gmail.com>
2010-03-19 00:24:14 +0300

    Rewrite javascript code according to NodeJS style guide. Now it passing through nodelint.js.

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -5,53 +5,52 @@ Copyright (C) 2010, Oleg Efimov <efimovov@gmail.com>
 See license text in LICENSE file
 */
 
-var host = "localhost";
-var user = "test";
-var password = "";
-var database = "test";
-var test_table = "test_table";
+var host = "localhost",
+  user = "test",
+  password = "",
+  database = "test",
+  test_table = "test_table",
 
+  // Operations count for continuous benchmarks
+  reconnects_count = 100000,
+  escapes_count = 10000000,
+  inserts_count = 200000,
+  selects_count = 200,
+  rows_to_select = 10,
 
-var reconnects_count = 100000;
-var escapes_count = 10000000;
-var inserts_count = 200000;
-var selects_count = 200;
-var rows_to_select = 10;
+  sys = require("sys"),
+  mysql_sync = require("../mysql-sync"),
+  
+  start,
+  finish,
 
-var sys = require("sys");
-var mysql_sync = require("../mysql-sync");
-
-
-var conn;
+  conn;
 
 
 // Utility functions
 
-function round2(number)
-{
-    return Math.round(number*100)/100;
+function round2(number) {
+  return Math.round(number * 100) / 100;
 }
 
-function do_benchmark(title, prepare_function, execute_function)
-{
-    var start, finish;
+function do_benchmark(title, prepare_function, execute_function) {
+  var start,
+    finish,
+    data;
     
-    if( typeof prepare_function == "function" )
-    {
-        var data = prepare_function();
+  if (typeof prepare_function === "function") {
+    data = prepare_function();
         
-        start = new Date();
-        execute_function(data);
-        finish = new Date();
-    }
-    else
-    {
-        start = new Date();
-        execute_function();
-        finish = new Date();
-    }
+    start = new Date();
+    execute_function(data);
+    finish = new Date();
+  } else {
+    start = new Date();
+    execute_function();
+    finish = new Date();
+  }
     
-    sys.puts(title + ": " + round2((finish.valueOf() - start.valueOf())/1000) + "s");
+  sys.puts(title + ": " + round2((finish.valueOf() - start.valueOf()) / 1000) + "s");
 }
 
 
@@ -61,97 +60,88 @@ sys.puts("Init...");
 
 conn = mysql_sync.createConnection(host, user, password, database);
 
-var start = new Date();
+start = new Date();
 
 
 // Benchmark 1: connect and disconnect
 
-do_benchmark("Connect and disconnect(" + reconnects_count + ")", 0, function(){
-
-    for( var i = 0; i < reconnects_count; i++ )
-    {
-        conn.close();
-        conn.connect(host, user, password, database);
-    }
-
+do_benchmark("Connect and disconnect(" + reconnects_count + ")", 0, function () {
+  var i;
+  for (i = 0; i < reconnects_count; i += 1) {
+    conn.close();
+    conn.connect(host, user, password, database);
+  }
 });
 
 
 // Benchmark 2: escape string
 
-do_benchmark("Escape string(" + escapes_count + ")", 0, function(){
+do_benchmark("Escape string(" + escapes_count + ")", 0, function () {
+  // A bit utility code
+  var string_to_escape = "test\\string\nwith\rmany\"symbols'and\x00nulls",
+    escaped_string,
+    i;
 
-    // A bit utility code
-    var string_to_escape = "test\\string\nwith\rmany\"symbols'and\x00nulls";
-    var escaped_string;
-
-    for( var i = 0; i < escapes_count; i++ )
-    {
-        escaped_string = conn.escape(string_to_escape);
-    }
-
+  for (i = 0; i < escapes_count; i += 1) {
+    escaped_string = conn.escape(string_to_escape);
+  }
 });
 
 
 // Benchmark 3: inserts
 
-do_benchmark("Inserts(" + inserts_count + ")", function(){
+do_benchmark("Inserts(" + inserts_count + ")", function () {
+  conn.query("DROP TABLE IF EXISTS " + test_table + ";");
+  conn.query("CREATE TABLE " + test_table + " (autoincrement_id BIGINT NOT NULL AUTO_INCREMENT, random_number INT(8) NOT NULL, random_boolean BOOLEAN NOT NULL, PRIMARY KEY (autoincrement_id));");
 
-    conn.query("DROP TABLE IF EXISTS " + test_table + ";");
-    conn.query("CREATE TABLE " + test_table + " (autoincrement_id BIGINT NOT NULL AUTO_INCREMENT, random_number INT(8) NOT NULL, random_boolean BOOLEAN NOT NULL, PRIMARY KEY (autoincrement_id));");
+  var rows = [],
+    i;
 
-    var rows = [];
-
-    for( var i = 0; i < inserts_count; i++ )
-    {
-        rows[i] = { 'random_number': Math.round(Math.random()*1000000), 'random_boolean': ( (Math.random() > 0.5) ? 1 : 0 ) };
-    }
+  for (i = 0; i < inserts_count; i += 1) {
+    rows[i] = {
+      'random_number': Math.round(Math.random() * 1000000),
+      'random_boolean': (Math.random() > 0.5) ? 1 : 0
+    };
+  }
     
-    return rows;
-
-}, function(rows){
-
-    for( var i = 0; i < inserts_count; i++ )
-    {
-        conn.query("INSERT INTO " + test_table + " (random_number, random_boolean) VALUES ('" + rows[i].random_number + "', '" + rows[i].random_boolean + "');");
-    }
-
+  return rows;
+}, function (rows) {
+  var i;
+  for (i = 0; i < inserts_count; i += 1) {
+    conn.query("INSERT INTO " + test_table + " (random_number, random_boolean) VALUES ('" + rows[i].random_number + "', '" + rows[i].random_boolean + "');");
+  }
 });
 
 
 // Benchmark 3: selects
 
-do_benchmark("Selects(" + selects_count + ", " + rows_to_select + ")", function(){
+do_benchmark("Selects(" + selects_count + ", " + rows_to_select + ")", function () {
+  var limits = [],
+    i;
 
-    var limits = [];
-
-    for( var i = 0; i < selects_count; i++ )
-    {
-        limits[i] = Math.round(Math.random()*(inserts_count-rows_to_select));
-    }
+  for (i = 0; i < selects_count; i += 1) {
+    limits[i] = Math.round(Math.random() * (inserts_count - rows_to_select));
+  }
     
-    return limits;
-
-}, function(limits){
-
-    var rows = new Array(selects_count);
+  return limits;
+}, function (limits) {
+  var rows = new Array(selects_count),
+    i;
     
-    for( var i = 0; i < selects_count; i++ )
-    {
-        conn.query("SELECT * FROM " + test_table + " ORDER BY RAND() LIMIT " + limits[i] + ", " + rows_to_select + ";");
-        rows[i] = conn.fetchResult();
-    }
-
+  for (i = 0; i < selects_count; i += 1) {
+    conn.query("SELECT * FROM " + test_table + " ORDER BY RAND() LIMIT " + limits[i] + ", " + rows_to_select + ";");
+    rows[i] = conn.fetchResult();
+  }
 });
 
 
 // Finish benchmarks
 
-var finish = new Date();
+finish = new Date();
 
 sys.puts("Finish.");
 
 conn.close();
 
-sys.puts("Full time: " + round2((finish.valueOf() - start.valueOf())/1000) + "s");
+sys.puts("Full time: " + round2((finish.valueOf() - start.valueOf()) / 1000) + "s");
 
--- tests/test.js
+++ tests/test.js
@@ -6,97 +6,103 @@ See license text in LICENSE file
 */
 
 // Database connection parameters
-var host = "localhost";
-var user = "test";
-var password = "";
-var database = "test";
-var database_denied = "mysql";
-var test_table = "test_table";
+var host = "localhost",
+  user = "test",
+  password = "",
+  database = "test",
+  database_denied = "mysql",
+  test_table = "test_table",
 
-// Operations count for continuous tests
-var reconnect_count = 10000;
-var insert_rows_count = 10000;
+  // Operations count for continuous tests
+  reconnect_count = 10000,
+  insert_rows_count = 10000,
 
-// Require modules
-var sys = require("sys");
-var mysql_sync = require("../mysql-sync");
-var unittest = require("./unittest").unittest;
+  // Require modules
+  sys = require("sys"),
+  mysql_sync = require("../mysql-sync"),
+  unittest = require("./unittest").unittest,
 
-var conn;
-var flag;
+  conn,
+  flag;
 
 sys.print("Running tests for NodeJS syncronous MySQL binding...\n");
 
-unittest.test('mysql_sync.createConnection()', function() {
-    var conn = mysql_sync.createConnection(host, user, password, database);
-    
-    unittest.assert(conn);
-    
-    conn.close();
+unittest.test('mysql_sync.createConnection()', function () {
+  var conn = mysql_sync.createConnection(host, user, password, database);
+  
+  unittest.assert(conn);
+  
+  conn.close();
 });
 
 /*
 // TODO: how to write this test?
 unittest.test('conn.close()', function() {
-    var conn = mysql_sync.createConnection(host, user, password, database);
-    conn.close();
-    unittest.assert(conn);
+  var conn = mysql_sync.createConnection(host, user, password, database);
+  conn.close();
+  unittest.assert(conn);
 });
 */
 
-unittest.test('conn.connect() for allowed database', function() {
-    var conn = mysql_sync.createConnection(host, user, password, database);
-    
-    conn.close();
-    unittest.assert(conn.connect(host, user, password, database));
-    
-    conn.close();
+unittest.test('conn.connect() for allowed database', function () {
+  var conn = mysql_sync.createConnection(host, user, password, database);
+  
+  conn.close();
+  unittest.assert(conn.connect(host, user, password, database));
+  
+  conn.close();
 });
 
-unittest.test('conn.connect() for denied database', function() {
-    var conn = mysql_sync.createConnection(host, user, password, database);
-    
-    conn.close();
-    
-    unittest.assert(!conn.connect(host, user, password, database_denied));
+unittest.test('conn.connect() for denied database', function () {
+  var conn = mysql_sync.createConnection(host, user, password, database);
+  
+  conn.close();
+  
+  unittest.assert(!conn.connect(host, user, password, database_denied));
 });
 
-unittest.test('conn.getInfo()', function() {
-    var conn = mysql_sync.createConnection(host, user, password, database);
-    
-    var info = conn.getInfo();
-    unittest.assert(info);
-    for( var i in info )
-    {
-        sys.puts("info['" + i + "'] = " + info[i]);
+unittest.test('conn.getInfo()', function () {
+  var conn = mysql_sync.createConnection(host, user, password, database),
+    info = conn.getInfo(),
+    i;
+  
+  unittest.assert(info);
+  for (i in info) {
+    if (typeof i === 'string') {
+      sys.puts("info['" + i + "'] = " + info[i]);
     }
-    
-    conn.close();
+  }
+  
+  conn.close();
 });
 
-unittest.test('conn.escape()', function() {
-    var conn = mysql_sync.createConnection(host, user, password, database);
-    
-    var strings_to_escape = [
-        ["test string", "test string"],
-        ["test\\string", "test\\\\string"],
-        ["test\nstring", "test\\nstring"],
-        ["test\rstring", "test\\rstring"],
-        ["test\"string", "test\\\"string"],
-        ["test\'string", "test\\'string"],
-        ["test \x00", "test \\0"],
-    ];
-    
-    for( var i in strings_to_escape )
-    {
-        var str = strings_to_escape[i][0];
-        var str_esc_theor = strings_to_escape[i][1];
-        var str_esc_real = conn.escape(strings_to_escape[i][0]);
-        
-        unittest.assertEqual(str_esc_theor, str_esc_real);
+unittest.test('conn.escape()', function () {
+  var conn = mysql_sync.createConnection(host, user, password, database),
+    strings_to_escape = [
+    ["test string", "test string"],
+    ["test\\string", "test\\\\string"],
+    ["test\nstring", "test\\nstring"],
+    ["test\rstring", "test\\rstring"],
+    ["test\"string", "test\\\"string"],
+    ["test\'string", "test\\'string"],
+    ["test \x00", "test \\0"]
+  ],
+  str,
+  str_esc_theor,
+  str_esc_real,
+  i;
+  
+  for (i in strings_to_escape) {
+    if (typeof i === 'string') {
+      str = strings_to_escape[i][0];
+      str_esc_theor = strings_to_escape[i][1];
+      str_esc_real = conn.escape(strings_to_escape[i][0]);
+
+      unittest.assertEqual(str_esc_theor, str_esc_real);
     }
-    
-    conn.close();
+  }
+  
+  conn.close();
 });
 
 unittest.showResults();
@@ -112,7 +118,7 @@ var show_tables_result = conn.fetchResult();
 sys.print("OK\n");
 for( var i in show_tables_result )
 {
-    sys.puts(i + ": " + JSON.stringify(show_tables_result[i]));
+  sys.puts(i + ": " + JSON.stringify(show_tables_result[i]));
 }
 
 sys.print("Drop table " + test_table + ": ");
@@ -126,9 +132,9 @@ sys.print("OK\n");
 sys.print("Insert " + insert_rows_count + " rows into table " + test_table + ": ");
 for( var i = 0; i < insert_rows_count; i++ )
 {
-    var random_number = Math.round(Math.random()*1000000);
-    var random_boolean = (Math.random() > 0.5) ? 1 : 0;
-    conn.query("INSERT INTO " + test_table + " (random_number, random_boolean) VALUES ('" + random_number + "', '" + random_boolean + "');");
+  var random_number = Math.round(Math.random()*1000000);
+  var random_boolean = (Math.random() > 0.5) ? 1 : 0;
+  conn.query("INSERT INTO " + test_table + " (random_number, random_boolean) VALUES ('" + random_number + "', '" + random_boolean + "');");
 }
 sys.print("OK\n");
 
@@ -136,11 +142,11 @@ sys.print("Test conn.lastInsertId(): ");
 var insert_id = conn.lastInsertId();
 if( insert_id == insert_rows_count )
 {
-    sys.print(" OK\n");
+  sys.print(" OK\n");
 }
 else
 {
-    sys.print(" FAILED [" + insert_id + " != " + insert_rows_count + "]\n");
+  sys.print(" FAILED [" + insert_id + " != " + insert_rows_count + "]\n");
 }
 
 sys.print("Run end get results of query 'SELECT * FROM " + test_table + " ORDER BY RAND() LIMIT 10': ");
@@ -149,7 +155,7 @@ var select_limit_result = conn.fetchResult();
 sys.print("OK\n");
 for( var i in select_limit_result )
 {
-    sys.puts(i + ": " + JSON.stringify(select_limit_result[i]));
+  sys.puts(i + ": " + JSON.stringify(select_limit_result[i]));
 }
 
 sys.print("conn.warningCount() test: ");
@@ -162,8 +168,9 @@ sys.print(" OK\n");
 sys.print("Reconnect to database " + reconnect_count + " times: ");
 for( var i = 1; i <= reconnect_count; i++ )
 {
-    conn.close();
-    conn.connect(host, user, password, database);
+  conn.close();
+  conn.connect(host, user, password, database);
 }
 sys.print("OK\n");
 */
+
