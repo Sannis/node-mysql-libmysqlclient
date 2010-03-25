c9ae99584234f5a5688abf357c28503f35ea6158
Oleg Efimov <efimovov@gmail.com>
2010-03-25 17:33:16 +0300

    Add tests for query() and lastInsertId() methods

--- tests/test-queries.js
+++ tests/test-queries.js
@@ -6,7 +6,7 @@ See license text in LICENSE file
 */
 
 // Mixin settings
-/*global host, user, password, database, database_denied */
+/*global host, user, password, database, database_denied, test_table, insert_rows_count */
 process.mixin(require("./settings"));
 
 // Require modules
@@ -48,61 +48,74 @@ exports.escape = function (test) {
   test.done();
 };
 
-/*
-unittest.test('conn.query("SHOW TABLES;")', function () {
+exports.query_ShowTables = function (test) {
+  test.expect(2);
+  
   var conn = mysql_sync.createConnection(host, user, password, database),
     res;
-  
+  test.ok(conn, "mysql_sync.createConnection(host, user, password, database)");
   res = conn.query("SHOW TABLES;");
-  unittest.assert(res);
-  
+  test.ok(res, "conn.query('SHOW TABLES;'");
   conn.close();
-});
+  
+  test.done();
+};
 
-unittest.test('conn.query("SHOW TABLES;").fetchResult()', function () {
+exports.query_ShowTables_FetchResult = function (test) {
+  test.expect(3);
+  
   var conn = mysql_sync.createConnection(host, user, password, database),
     res,
     tables;
-  
+  test.ok(conn, "mysql_sync.createConnection(host, user, password, database)");
   res = conn.query("SHOW TABLES;");
+  test.ok(res, "conn.query('SHOW TABLES;')");
   tables = res.fetchResult();
-  
-  unittest.assert(tables);
-  
+  test.ok(tables, "res.fetchResult()");
   conn.close();
-});
+  
+  test.done();
+};
 
-unittest.test('Drop table ' + test_table, function () {
+exports.query_DropTestTable = function (test) {
+  test.expect(2);
+  
   var conn = mysql_sync.createConnection(host, user, password, database),
     res;
-  
+  test.ok(conn, "mysql_sync.createConnection(host, user, password, database)");
   res = conn.query("DROP TABLE IF EXISTS " + test_table + ";");
-
-  unittest.assertEqual(res, true);
-  
+  test.equals(res, true, "conn.query('DROP TABLE IF EXISTS ...;'");
   conn.close();
-});
+  
+  test.done();
+};
 
-unittest.test('Create table ' + test_table, function () {
+exports.query_CreateTestTable = function (test) {
+  test.expect(2);
+  
   var conn = mysql_sync.createConnection(host, user, password, database),
     res;
-  
+  test.ok(conn, "mysql_sync.createConnection(host, user, password, database)");
   res = conn.query("CREATE TABLE " + test_table +
     " (autoincrement_id BIGINT NOT NULL AUTO_INCREMENT," +
     " random_number INT(8) NOT NULL, random_boolean BOOLEAN NOT NULL," +
     " PRIMARY KEY (autoincrement_id));");
-
-  unittest.assertEqual(res, true);
-  
+  test.equals(res, true, "conn.query('CREATE TABLE ...;'");
   conn.close();
-});
+  
+  test.done();
+};
 
-unittest.test('Insert ' + insert_rows_count + ' rows into table ' + test_table, function () {
+exports.query_InsertIntoTestTable = function (test) {
+  test.expect(2);
+  
   var conn = mysql_sync.createConnection(host, user, password, database),
     res = true,
     random_number,
     random_boolean,
     i;
+  
+  test.ok(conn, "mysql_sync.createConnection(host, user, password, database)");
   
   for (i = 0; i < insert_rows_count; i += 1)
   {
@@ -113,12 +126,15 @@ unittest.test('Insert ' + insert_rows_count + ' rows into table ' + test_table, 
       "', '" + random_boolean + "');") && res;
   }
 
-  unittest.assertEqual(res, true);
-  
+  test.equals(res, true, "Insert " + insert_rows_count + " rows into table " + test_table);
   conn.close();
-});
+  
+  test.done();
+};
 
-unittest.test('conn.lastInsertId()', function () {
+exports.query_InsertIntoTestTable = function (test) {
+  test.expect(3);
+
   var conn = mysql_sync.createConnection(host, user, password, database),
     res = true,
     random_number,
@@ -126,6 +142,8 @@ unittest.test('conn.lastInsertId()', function () {
     last_insert_id,
     i;
   
+  test.ok(conn, "mysql_sync.createConnection(host, user, password, database)");
+  
   for (i = 0; i < insert_rows_count; i += 1)
   {
     random_number = Math.round(Math.random() * 1000000);
@@ -135,11 +153,42 @@ unittest.test('conn.lastInsertId()', function () {
       "', '" + random_boolean + "');") && res;
   }
 
+  test.equals(res, true, "Insert " + insert_rows_count + " rows into table " + test_table);
   last_insert_id = conn.lastInsertId();
-  
-  unittest.assertEqual(last_insert_id, 2 * insert_rows_count);
-  
+  // TODO: WTF? Asynchronouse?
+  test.equals(last_insert_id, insert_rows_count, "conn.lastInsertId() " + last_insert_id + " " + insert_rows_count);
   conn.close();
-});
+  
+  test.done();
+};
+
+/*
+// TODO: Rewrite these tests
+
+for( var i in show_tables_result )
+{
+  sys.puts(i + ": " + JSON.stringify(show_tables_result[i]));
+}
+
+sys.print("Test conn.lastInsertId(): ");
+var insert_id = conn.lastInsertId();
+if( insert_id == insert_rows_count )
+{
+  sys.print(" OK\n");
+}
+else
+{
+  sys.print(" FAILED [" + insert_id + " != " + insert_rows_count + "]\n");
+}
+
+sys.print("Run end get results of query 'SELECT * FROM " + test_table + " ORDER BY RAND() LIMIT 10': ");
+conn.query("SELECT * FROM " + test_table + " ORDER BY RAND() LIMIT 10;");
+var select_limit_result = conn.fetchResult();
+sys.print("OK\n");
+for( var i in select_limit_result )
+{
+  sys.puts(i + ": " + JSON.stringify(select_limit_result[i]));
+}
 */
+
 
