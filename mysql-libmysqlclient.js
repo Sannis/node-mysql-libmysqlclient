79e3830b75fee53b579762eafeb864b9cfd28dda
Oleg Efimov <efimovov@gmail.com>
2010-07-21 02:09:24 +0400

    Fix 2 failed tests, last patch changes some behavior

--- mysql-libmysqlclient.js
+++ mysql-libmysqlclient.js
@@ -8,6 +8,8 @@ var binding = require("./mysql_bindings");
 
 exports.createConnection = function () {
   var db = new binding.MysqlConn();
-  db.connect.apply(db, Array.prototype.slice.call(arguments, 0, 6));
+  if (arguments.length > 0) {
+    db.connect.apply(db, Array.prototype.slice.call(arguments, 0, 6));
+  }
   return db;
 };
