17bc0a8a93abac0d27cc74d18ec77984fc3e6573
Oleg Efimov <efimovov@gmail.com>
2010-08-28 02:09:55 +0400

    Move async methods tests into related file. Add tests file for datatypes casting.

--- tests/test-1-class-mysqlconn.js
+++ tests/test-1-class-mysqlconn.js
@@ -95,6 +95,32 @@ exports.AffectedRowsSync = function (test) {
   test.done();
 };
 
+exports.Connect = function (test) {
+  test.expect(1);
+  
+  var conn = new mysql_bindings.MysqlConn();
+  conn.connect(cfg.host, cfg.user, cfg.password, cfg.database, function (error) {
+    test.ok(error === null, "conn.connect() for allowed database");
+    
+    conn.closeSync();
+  
+    test.done();
+  });
+};
+
+exports.ConnectWithError = function (test) {
+  test.expect(1);
+  
+  var conn = new mysql_bindings.MysqlConn();
+  conn.connect(cfg.host, cfg.user, cfg.password, cfg.database_denied, function (error) {
+    test.ok(error === 1044, "conn.connect() for denied database");
+    
+    conn.closeSync();
+  
+    test.done();
+  });
+};
+
 exports.ConnectSync = function (test) {
   test.expect(2);
   
@@ -251,6 +277,39 @@ exports.LastInsertIdSync = function (test) {
   conn.closeSync();
   
   test.done();
+};
+
+exports.Query = function (test) {
+  var conn = mysql_libmysqlclient.createConnectionSync(cfg.host, cfg.user, cfg.password, cfg.database), res;
+
+  res = conn.querySync("DROP TABLE IF EXISTS " + cfg.test_table + ";");
+  res = conn.querySync("CREATE TABLE " + cfg.test_table +
+    " (autoincrement_id BIGINT NOT NULL AUTO_INCREMENT," +
+    " random_number INT(8) NOT NULL, random_boolean BOOLEAN NOT NULL," +
+    " PRIMARY KEY (autoincrement_id)) TYPE=MEMORY;");
+
+  test.expect(2);
+  conn.query("SHOW TABLES", function (err, result) {
+    test.ok(result.fieldCount === 1, "show results field count === 1");
+    var res = result.fetchAllSync();
+    test.ok(res.some(function (r) {
+      return r['Tables_in_' + cfg.database] === cfg.test_table;
+    }), "find the test table in results");
+    conn.closeSync();
+    test.done();
+  });
+};
+
+exports.QueryWithError = function (test) {
+  var conn = mysql_libmysqlclient.createConnectionSync(cfg.host, cfg.user, cfg.password, cfg.database), res;
+
+  test.expect(2);
+  conn.query("SHOW TABLESaagh", function (err, result) {
+    test.ok(!result, "result is not defined");
+    test.ok(err, "error object is present");
+    conn.closeSync();
+    test.done();
+  });
 };
 
 exports.QuerySync = function (test) {
