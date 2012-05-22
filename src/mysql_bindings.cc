1a9e65d3cef752ba5a3fbaf40a1c2664272f2c3d
Oleg Efimov <efimovov@gmail.com>
2012-05-23 01:24:41 +0400

    Backport node::MakeCallback from Node v0.7.8, refs #134

--- src/mysql_bindings.cc
+++ src/mysql_bindings.cc
@@ -14,6 +14,34 @@
 #include "./mysql_bindings_result.h"
 #include "./mysql_bindings_statement.h"
 
+/* Backport MakeCallback from Node v0.7.8 */
+#if NODE_VERSION_AT_LEAST(0, 7, 8)
+  // Nothing
+#else
+    namespace node {
+        Handle<Value>
+        MakeCallback(const Handle<Object> object,
+                     const Handle<Function> callback,
+                     int argc,
+                     Handle<Value> argv[]) {
+          HandleScope scope;
+
+          // TODO Hook for long stack traces to be made here.
+
+          TryCatch try_catch;
+
+          Local<Value> ret = callback->Call(object, argc, argv);
+
+          if (try_catch.HasCaught()) {
+            FatalException(try_catch);
+            return Undefined();
+          }
+
+          return scope.Close(ret);
+        }
+    }  // namespace node
+#endif
+
 /**
  * Init V8 structures
  *
