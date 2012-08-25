1e36d2d05f0bc8e4b63c7ab1de6b67a401ae9ced
Alexander Makarenko <estliberitas@gmail.com>
2012-08-25 05:52:02 -0400

    fix warning: "statement has no effect"

--- src/mysql_bindings_statement.h
+++ src/mysql_bindings_statement.h
@@ -34,7 +34,7 @@
 #ifdef DEBUG
     #define DEBUG_PRINT(fmt, ...) fprintf(stdout, fmt, __VA_ARGS__)
 #else
-    #define DEBUG_PRINT(fmt, ...) 1
+    #define DEBUG_PRINT(fmt, ...) (void)0
 #endif
 
 
